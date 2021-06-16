c6f1c3f4422798f3ccd986dae7abf636e1796124
Eugene Baulin <baulin@atp-fivt.org>
2021-06-16 11:28:32 +0300

    Rfam annotations added

--- urslib2/Rfam.py
+++ urslib2/Rfam.py
@@ -32,11 +32,13 @@ def UpdateRfam(folder,
 
 
 def GetRfamInfo(path_to_rfam_files = '',
-                update = False): 
+                update = False,
+                rfam_ftp = 'ftp://ftp.ebi.ac.uk/pub/databases/Rfam/CURRENT/database_files/'): 
     '''
     Parameters:
         path_to_rfam_files - path to family.txt and pdb_full_region.txt (by default - working directory)
         update - True/False - download new family.txt and pdb_full_region.txt
+        rfam_ftp - source URL for Rfam files
     Returns:
         dictionary {PDB-ID: {CHAIN: RFAM_INFO}}
     '''
@@ -47,24 +49,41 @@ def GetRfamInfo(path_to_rfam_files = '',
        not os.path.exists(path_to_pdb) or \
        update == True:
 
-        UpdateRfam(path_to_rfam_files)
+        UpdateRfam(path_to_rfam_files,rfam_ftp)
 
     rfam = {}
 
+    rfam_acc = {}
+
+    with open(path_to_family, errors='ignore') as file:
+
+        for line in file:
+
+            racc, rid, rwiki, rdesc = line.strip().split('\t')[:4]
+
+            rfam_acc[racc] = (rid,rdesc)
+
+    with open(path_to_pdb) as file:
+
+        for line in file:
+
+            racc, pdbid, chain = line.strip().split('\t')[:3]
+
+            if pdbid not in rfam:
+
+                rfam[pdbid] = {}
+
+            if chain not in rfam[pdbid]:
+
+                rfam[pdbid][chain] = [racc,*rfam_acc[racc]]
 
     return rfam    
 
-    
-    
-
-    
-
-    
 
 
 if __name__=='__main__':
 
     folder = ''
     
-    GetRfamInfo(folder)
+    rfam = GetRfamInfo(folder)
 
